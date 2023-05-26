cfb6c9c1d1ccdf8a989eb4347af9cc58dbc09b3c
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-05-26 18:16:30 +0600

    table added for my toys page

--- src/components/MyToys/MyToys.jsx
+++ src/components/MyToys/MyToys.jsx
@@ -2,21 +2,57 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider/AuthProvider';
 
 const MyToys = () => {
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     console.log(user)
     const [myToys, setMyToys] = useState([])
 
     let url = `http://localhost:5000/mytoys?sellerEmail=${user?.email}`
-    useEffect(()=>{
+    useEffect(() => {
         fetch(url)
-        .then(res => res.json())
-        .then(data => setMyToys(data))
+            .then(res => res.json())
+            .then(data => setMyToys(data))
     }, [url])
     console.log(myToys)
     return (
-        
+
         <div>
             <h2>My Toys page: {myToys.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head*/}
+                    <thead>
+                        <tr>
+                            <th>NO</th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        {/* row 2 */}
+                        <tr className="active">
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
